102cb48443f9b0702bb3085a6b9a0f431b18e341
JamesP64 <jamespalmer2005@icloud.com>
2025-11-05 22:38:23 +0000

    Fade in Light to show glass box move

--- COMP3218/Assets/Scripts/Level3/Level3PDetect.cs
+++ COMP3218/Assets/Scripts/Level3/Level3PDetect.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.Rendering.Universal;
 
 public class Level3PDetect : MonoBehaviour
 {
@@ -23,12 +25,18 @@ public class Level3PDetect : MonoBehaviour
 
     public Level3MeowDialogue dialogue;
 
+    public Sprite downSprite;
+    public Sprite upSprite;
+    public SpriteRenderer spriteRenderer;
+
+    public GameObject boxShowLight;
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -41,6 +49,7 @@ public class Level3PDetect : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
+            spriteRenderer.sprite = downSprite;
             if (boxPos1.activeSelf)
             {
                 // Swap Box Sprites
@@ -64,7 +73,9 @@ public class Level3PDetect : MonoBehaviour
                     boxPos2SafeZoneOccluded.SetActive(true);
                     boxPos2SafeZoneNotOccluded.SetActive(false);
                 }
-            }else
+                StartCoroutine(FlickerLight(boxShowLight));
+            }
+            else
             {
                 boxPos1.SetActive(!boxPos1.activeSelf);
                 boxPos2.SetActive(!boxPos2.activeSelf);
@@ -82,7 +93,41 @@ public class Level3PDetect : MonoBehaviour
                     boxPos1SafeZoneOccluded.SetActive(true);
                     boxPos1SafeZoneNotOccluded.SetActive(false);
                 }
+                boxShowLight.SetActive(false);
             }
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        spriteRenderer.sprite = upSprite;
+    }
+
+    private IEnumerator FlickerLight(GameObject target)
+    {
+        if (target == null) yield break;
+
+        Light2D light = target.GetComponent<Light2D>();
+        if (light == null) yield break;
+
+        target.SetActive(true);
+
+        float targetIntensity = 1.5f;
+        float duration = 1.2f;
+        float elapsed = 0f;
+
+        light.intensity = 0f;
+
+        yield return new WaitForSeconds(0.1f);
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / duration;
+
+            light.intensity = Mathf.Lerp(0f, targetIntensity, Mathf.SmoothStep(0f, 1f, t));
+            yield return null;
+        }
+
+        light.intensity = targetIntensity;
     }
 }
