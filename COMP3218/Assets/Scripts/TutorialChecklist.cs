dc79b7a53d1075369a37af79892469d47b637952
khart-m <khartmail05@gmail.com>
2025-11-06 15:33:37 +0000

    updated tutorial

--- COMP3218/Assets/Scripts/TutorialChecklist.cs
+++ COMP3218/Assets/Scripts/TutorialChecklist.cs
@@ -12,6 +12,15 @@ public class TutorialChecklist : MonoBehaviour
     public bool moveCompleted = false;
     public bool meowCompleted = false;
 
+    private bool wPressed = false;
+    private bool aPressed = false;
+    private bool sPressed = false;
+    private bool dPressed = false;
+    public GameObject moveTaskLabel;
+    public GameObject meowTaskLabel;
+    public GameObject exitTaskLabel;
+    public Color completedColour;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -26,13 +35,38 @@ public class TutorialChecklist : MonoBehaviour
                                Keyboard.current.sKey.wasPressedThisFrame ||
                                Keyboard.current.dKey.wasPressedThisFrame ))
         {
+            if (Keyboard.current.wKey.wasPressedThisFrame)
+            {
+                wPressed = true;
+            }
+            if (Keyboard.current.aKey.wasPressedThisFrame)
+            {
+                aPressed = true;
+            }
+            if (Keyboard.current.sKey.wasPressedThisFrame)
+            {
+                sPressed = true;
+            }
+            if (Keyboard.current.dKey.wasPressedThisFrame)
+            {
+                dPressed = true;
+            }
+        }
+
+        if(!moveCompleted && wPressed && aPressed && dPressed && sPressed) {
             moveCompleted = true;
+            // change transparency of label
+            moveTaskLabel.GetComponent<Text>().color = completedColour;
+            meowTaskLabel.SetActive(true);
             moveTask.isOn = true;
         }
 
         if (moveCompleted && !meowCompleted && Keyboard.current.spaceKey.wasPressedThisFrame)
         {
             meowCompleted = true;
+            // change transparency of label
+            meowTaskLabel.GetComponent <Text>().color = completedColour;
+            exitTaskLabel.SetActive(true);
             meowTask.isOn = true;
         }
 
